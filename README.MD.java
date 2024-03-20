351d7baf3f11e8ca0187408962593d9832b102b8
EnjoyGamer3698 <160103945+EnjoyGamer3698@users.noreply.github.com>
2024-03-20 17:50:58 +0800

    Add README.MD
    I know it's strange, but apart from the file extension, it seems like there's no problem

---
+++ README.MD.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Why are you want to open it?");
+        System.out.println("It's just a java file -_-");
+    }
+}
